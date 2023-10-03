2c01f5eb3ee746764fb28aaf7e2417aff6826d20
SilverX [sx66] <serainox@gmail.com>
2023-10-03 14:22:35 +0200

    Add files via upload

---
+++ webui/hookx.py
@@ -0,0 +1,51 @@
+import argparse
+import requests
+import mimetypes
+
+# Replace this with your Discord webhook URL
+WEBHOOK_URL = "https://discord.com/api/webhooks/1158739168013332571/gwQPV7-Orm2omUsBK-_zmOe6N4UsVnoSXMN6KQ2iUcit3u4wwueTi_WTEVIvKiHhDyBY"
+
+def send_message_to_discord(message, file_paths=[]):
+    payload = {"content": message}
+    files = []
+
+    for file_path in file_paths:
+        mime_type, _ = mimetypes.guess_type(file_path)
+        if mime_type:
+            files.append(("file", (file_path, open(file_path, "rb"), mime_type)))
+
+    response = requests.post(WEBHOOK_URL, data=payload, files=files)
+
+    if response.status_code == 204:
+        print("Message sent successfully to Discord.")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
+        print(response.text)
+
+def main():
+    parser = argparse.ArgumentParser(description="Send messages and media files to a Discord webhook.")
+    parser.add_argument("--message", "-m", help="The message to send to Discord.")
+    parser.add_argument("--file", "-f", nargs="*", help="The path to media files to send to Discord.")
+    parser.add_argument("--log", "-l", help="Log messages to a file.")
+
+    args = parser.parse_args()
+
+    if args.message:
+        send_message_to_discord(args.message)
+    elif args.file:
+        send_message_to_discord("Uploaded media:", args.file)
+    elif args.log:
+        try:
+            with open(args.log, "a") as log_file:
+                while True:
+                    message = input("Enter a message to log (or 'q' to quit): ")
+                    if message.lower() == "q":
+                        break
+                    log_file.write(message + "\n")
+        except KeyboardInterrupt:
+            pass
+    else:
+        print("Please provide a message, file, or log option. Use -h for help.")
+
+if __name__ == "__main__":
+    main()
